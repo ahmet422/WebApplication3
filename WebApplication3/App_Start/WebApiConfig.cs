5aed5fe12bcc10ed8c8cabdf4b0130927a929000
ahmet422 <ahmet.tajik@icloud.com>
2021-02-03 14:32:09 -0600

    Add project files.

---
+++ WebApplication3/App_Start/WebApiConfig.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WebApiContrib.Formatting.Jsonp;
+
+namespace WebApplication3
+{
+    public static class WebApiConfig
+    {
+        //  return JSON instead of XML from ASP.NET Web API Service when a request is made from the browser.#2
+
+        //public class CustomJsonFormatter : JsonMediaTypeFormatter
+        //{
+        //    public CustomJsonFormatter()
+        //    {
+        //        this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        //    }
+
+        //    public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+        //    {
+        //        base.SetDefaultContentHeaders(type, headers, mediaType);
+        //        headers.ContentType = new MediaTypeHeaderValue("application/json");
+        //    }
+        //}
+
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Filters.Add(new RequireHttpsAttribute());
+
+            // so we can use Cross origin resource sharing * means inlcude every option
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+         //   config.EnableCors(cors);
+            config.EnableCors();
+
+            // so we can call Web API service in a cross domain using jQuery ajax
+            //var jsonpFormatter = new JsonpMediaTypeFormatter(config.Formatters.JsonFormatter);
+            //config.Formatters.Insert(0, jsonpFormatter);
+
+
+            //continuation of line 16
+            //    config.Formatters.Add(new CustomJsonFormatter());
+
+            //  return JSON instead of XML from ASP.NET Web API Service when a request is made from the browser #1. 
+            //config.Formatters.JsonFormatter.SupportedMediaTypes
+            //.Add(new MediaTypeHeaderValue("text/html"));
+
+            // supporting only json and not xml
+            //   config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            // to indend the sending data properly we do:
+            //   config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+
+            // Camel case instead of Pascal case
+            //   config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
+  
---
+++ WebApplication3/Controllers/EmployeesController.cs
@@ -0,0 +1,140 @@
+﻿using EmployeeDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApplication3.Controllers
+{
+    [EnableCorsAttribute("*", "*", "*")]
+    public class EmployeesController : ApiController
+    {
+
+        [HttpGet]
+        [BasicAuthentication]
+        public HttpResponseMessage Get(string gender = "All")
+        {
+            string username = Thread.CurrentPrincipal.Identity.Name;
+
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                switch (username.ToLower())
+                {
+                    case "male":
+                        return Request.CreateResponse(HttpStatusCode.OK, 
+                            entities.Employees.Where(emp => emp.Gender.ToLower() == "male").ToList());
+                    case "female":
+                        return Request.CreateResponse(HttpStatusCode.OK,
+                            entities.Employees.Where(emp => emp.Gender.ToLower() == "female").ToList());
+                    default:
+                        return Request.CreateResponse(HttpStatusCode.BadRequest);
+                }
+            }
+        }
+
+        [HttpGet]
+        public HttpResponseMessage LoadEmployeeById(int id)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                var entity = entities.Employees.FirstOrDefault(emp => emp.Id == id);
+
+                if (entity != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with id: " + id.ToString() +  " not found" );
+                }
+            }
+        }
+
+        public HttpResponseMessage Post([FromBody] Employee employee)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    entities.Employees.Add(employee);
+                    entities.SaveChanges();
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri + "/" + employee.Id.ToString());
+                    return message;
+                }
+            }
+
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+            
+
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(emp => emp.Id == id);
+
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with id: " + id.ToString() + " not found to delete");
+                    }
+                    else
+                    {
+                        entities.Employees.Remove(entity);
+                        entities.SaveChanges();
+
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+        }
+
+        public HttpResponseMessage Put(int id, [FromUri] Employee employee)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(emp => emp.Id == id);
+
+                    if (entity != null)
+                    {
+                        entity.FisrtName = employee.FisrtName;
+                        entity.LastName = employee.LastName;
+                        entity.Gender = employee.Gender;
+                        entity.Salary = employee.Salary;
+
+                        entities.SaveChanges();
+
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    }
+                    else
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with id: " + id.ToString() + " not found to update");
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+            }
+            
+        }
+    }
+}
